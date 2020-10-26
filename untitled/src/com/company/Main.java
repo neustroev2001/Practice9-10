2dc21a51d43d3e2b6fc4f75b66209a86256f30db
Vlad <neustroev.leonid@mail.ru>
2020-10-26 17:01:42 +0300

    Initial commit

---
+++ untitled/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	a = scanner.nextInt();
+	int a = 1;
+	float b = 2.2f;
+	boolean c = false;
+
+	String string = "qwert yuio"; // объект
+	String[] stringArray = string.split(s = " ");
+            char g = 'd';
+            a =  Integer.parseInt(scanner.nextLine());
+            System.out.println(a+1);
+
+    }
+}
